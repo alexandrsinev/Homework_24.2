3ca161f6647e22787e13499915817c61e950b94e
Alexandr Sinev <sir.sineow@yandex.ru>
2024-06-26 02:22:27 +0300

    написаны отображение для пользователей и отображение для платежей с фильтрацией и сортировкой

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics
+from rest_framework.filters import OrderingFilter
 
-# Create your views here.
+from users.models import Users, Payments
+from users.serializers import UserSerializer, PaymentSerializer
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
+
+
+class PaymentsListAPIView(generics.ListAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method',)
+    ordering_fields = ('date_of_payment',)
+
