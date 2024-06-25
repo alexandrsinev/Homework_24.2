d496fae671a0ef6768adf3a3345919297e396788
Alexandr Sinev <sir.sineow@yandex.ru>
2024-06-26 02:21:01 +0300

    написаны urls для пользователей и отображения списка платежей

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
 from users.apps import UsersConfig
+from users.views import UsersViewSet, PaymentsListAPIView
 
 app_name = UsersConfig.name
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'users', UsersViewSet, basename='users')
+
+
+urlpatterns = [path('payments/', PaymentsListAPIView.as_view(), name='payments-list')] + router.urls
