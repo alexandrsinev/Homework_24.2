13d98b734d5bda955c3092bbac565a87da0f0123
Alexandr Sinev <sir.sineow@yandex.ru>
2024-07-02 01:13:56 +0300

    Во ViewSet пользователя написана функция создания нового пользователя и хеширования его пароля

--- users/views.py
+++ users/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from users.models import Users, Payments
+
 from users.serializers import UserSerializer, PaymentSerializer
 
 
@@ -11,11 +14,15 @@ class UsersViewSet(viewsets.ModelViewSet):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
-class PaymentsListAPIView(generics.ListAPIView):
+
+class PaymentsListAPIView(ListAPIView):
     queryset = Payments.objects.all()
     serializer_class = PaymentSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('paid_course', 'paid_lesson', 'payment_method',)
     ordering_fields = ('date_of_payment',)
-
