5176e70268f0919c377dc49d309679982c645cb4
Alexandr Sinev <sir.sineow@yandex.ru>
2024-07-07 22:10:35 +0300

    Добавлен класс для подписки и пагинация для курса и уроков

--- materials/views.py
+++ materials/views.py
@@ -1,17 +1,23 @@
+from django.http import response
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, \
+    get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
+from materials.paginators import MaterialsPagination
 from materials.permissions import IsUser
-from materials.serialaizers import CourseSerializer, LessonSerializer
+from materials.serialaizers import CourseSerializer, LessonSerializer, SubscriptionSerializer
 from materials.permissions import IsModer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    pagination_class = MaterialsPagination
 
     def perform_create(self, serializer):
         course = serializer.save()
@@ -20,18 +26,18 @@ class CourseViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action == 'create':
-            self.permission_classes = (IsAuthenticated, ~IsModer,)
+            self.permission_classes = [AllowAny, ~IsModer,]
         if self.action == 'destroy':
-            self.permission_classes = (IsAuthenticated, ~IsModer | IsUser)
+            self.permission_classes = [IsAuthenticated, ~IsModer | IsUser]
         if self.action in ['update', 'partial_update', 'list', 'retrieve']:
-            self.permission_classes = (IsAuthenticated, IsModer | IsUser)
+            self.permission_classes = [IsAuthenticated | IsModer,]
         return super().get_permissions()
 
 
 class LessonCreateAPIView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, ~IsModer]
+    permission_classes = [IsAuthenticated, ~IsModer | IsUser]
 
     def perform_create(self, serializer):
         lesson = serializer.save()
@@ -42,6 +48,7 @@ class LessonCreateAPIView(CreateAPIView):
 class LessonListAPIView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    pagination_class = MaterialsPagination
     permission_classes = [IsAuthenticated, IsModer | IsUser]
 
 
@@ -60,3 +67,23 @@ class LessonUpdateAPIView(UpdateAPIView):
 class LessonDeleteAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated, ~IsModer | IsUser]
+
+
+class SubscriptionAPIView(APIView):
+    serializer_class = SubscriptionSerializer
+
+    def post(self, *args, **kwargs):
+        user = self.request.user
+        course_id = self.request.data.get('course')
+        course_item = get_object_or_404(Course, pk=course_id)
+
+        subscription_item = Subscription.objects.filter(user=user, course=course_item)
+
+        if subscription_item.exists():
+            subscription_item.delete()
+            message = 'подписка удалена'
+        else:
+            Subscription.objects.create(user=user, course=course_item, active=True)
+            message = 'подписка добавлена'
+        return Response({"message": message})
+
