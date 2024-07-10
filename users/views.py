c6b8fae7a9e0b92cd2f5575e9a5ebfd1fcf31e9d
Alexandr Sinev <sir.sineow@yandex.ru>
2024-07-10 23:12:46 +0300

    Создан класс для возможности оплаты

--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from users.models import Users, Payments
 
 from users.serializers import UserSerializer, PaymentSerializer
+from users.services import create_price, create_stripe_sessions, create_stripe_product
 
 
 class UsersViewSet(viewsets.ModelViewSet):
@@ -26,3 +27,22 @@ class PaymentsListAPIView(ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('paid_course', 'paid_lesson', 'payment_method',)
     ordering_fields = ('date_of_payment',)
+    permission_classes = [IsAuthenticated, ]
+
+
+class PaymentCreateAPIView(CreateAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentSerializer
+
+    def perform_create(self, serializer):
+        print(self.request.user)
+        payment = serializer.save(user=self.request.user)
+        print(serializer)
+        print(payment)
+        print(payment.paid_course)
+        product = create_stripe_product(payment.paid_course)
+        price = create_price(product=product, payment_amount=payment.payment_amount)
+        session_id, payment_link = create_stripe_sessions(price)
+        payment.id_session = session_id
+        payment.link = payment_link
+        payment.save()
