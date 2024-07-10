bd78dd0e4462c499c2c953ba0b73adecb68fec0a
Alexandr Sinev <sir.sineow@yandex.ru>
2024-07-10 23:04:53 +0300

    В UserSerializer добавлено поле payment_history

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,17 @@ from rest_framework.serializers import ModelSerializer
 from users.models import Users, Payments
 
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = Users
-        fields = ('first_name', 'email',)
-
-
 class PaymentSerializer(ModelSerializer):
     class Meta:
         model = Payments
+        fields = '__all__'
+
+
+class UserSerializer(ModelSerializer):
+    payment_history = PaymentSerializer(
+        source="user_payment", many=True, read_only=True
+    )
+
+    class Meta:
+        model = Users
         fields = '__all__'
